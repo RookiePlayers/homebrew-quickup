84c78df618302c3827fdc7b22183069f7dce2fa8
ollie2224 <olamidepeters@gmail.com>
2025-10-31 15:56:18 +0000

    Fix URL in Quickup formula to point to the correct script location

--- Formula/quickup.rb
+++ Formula/quickup.rb
@@ -1,7 +1,7 @@
 class Quickup < Formula
     desc "One-shot dev workspace bootstrapper"
     homepage "https://github.com/RookiePlayers/Quickup.git"
-    url "https://github.com/RookiePlayers/Quickup/setup_workspace.sh"
+    url "https://github.com/RookiePlayers/Quickup/main/setup_workspace.sh"
     version "1.0.0"
     sha256 "a46c8038631bcdd1a064a8b4109cb6ece60a8a427882f473c7dffd0aecb4f74d"
     license "MIT"
